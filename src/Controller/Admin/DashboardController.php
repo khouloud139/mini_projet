224f4f822f1e8e101ba3f958a7d6c5a4f507edb1
NadaTrabelsi29 <nadatrabelsi29920@gmail.com>
2023-04-27 01:00:51 +0200

    update

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use App\Entity\Demande;
+use App\Entity\Message;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -50,5 +51,6 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
          yield MenuItem::linkToCrud('Users', 'fa fa-users', User::class);
          yield MenuItem::linkToCrud('Demandes', 'fa fa-bell', Demande::class);
+         yield MenuItem::linkToCrud('Messages', 'fa fa-message', Message::class);
     }
 }
